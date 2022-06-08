120d6d7cdd1b132372b12e1f1ef84378cf33dccb
akinwilson <akinola.antony.wilson@gmail.com>
2022-06-08 11:00:46 +0100

    setup.py file used to make package installable

---
+++ setup.py
@@ -0,0 +1,7 @@
+'''File allows for install of extractor package in editable mode'''
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        include_package_data=True,
+    )
