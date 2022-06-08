1758d6128c356ad9d6ccd08ecf080eb293602a32
akinwilson <akinola.antony.wilson@gmail.com>
2022-06-08 10:48:14 +0100

    added logging cofiguration such that when gunicorn manages processes of uvicorn, the access logs are logged by container

--- app/main.py
+++ app/main.py
@@ -1,18 +1,37 @@
-#!/usr/bin/env python3 
+#!/usr/bin python
 
 import os 
 import sys 
 
 import uvicorn 
+import logging 
+
+
+
 
 from vocabConstructor import Retriever, TextProcessor
 
 from fastapi import FastAPI, Request
-from fastapi.logger import logger 
+from fastapi.logger import logger  as fastapi_logger
 from fastapi.exceptions import RequestValidationError
 from exception_handler import python_exception_handler, validation_exception_handler
 
+
 from schema import * 
+
+
+gunicorn_error_logger = logging.getLogger("gunicorn.error")
+gunicorn_logger = logging.getLogger("gunicorn")
+uvicorn_access_logger = logging.getLogger("uvicorn.access")
+uvicorn_access_logger.handlers = gunicorn_error_logger.handlers
+
+fastapi_logger.handlers = gunicorn_error_logger.handlers
+
+
+if __name__ != "__main__":
+    fastapi_logger.setLevel(gunicorn_logger.level)
+else:
+    fastapi_logger.setLevel(logging.DEBUG)
 
 
 app = FastAPI(title="Webpage vocab constructor", 
@@ -29,8 +48,8 @@ app.add_exception_handler(Exception, python_exception_handler)
                    500: {"model": EndpointError}})
 
 def extract_vocab(request:Request, body:EndpointInput):
-    logger.info("API extract called")
-    logger.info(f"Input: {body}")
+    fastapi_logger.info("API extract called")
+    fastapi_logger.info(f"Input: {body}")
     
     url = body.url 
     sort_type = body.sort_type or None 
@@ -40,7 +59,7 @@ def extract_vocab(request:Request, body:EndpointInput):
     text_processor = TextProcessor(text=text, sort_type=sort_type)
     vocab = text_processor()
 
-    logger.info(f"Constructed vocab: {vocab}")
+    fastapi_logger.info(f"Constructed vocab: {vocab}")
     result = {  
         "url": url, 
         "sort_type": sort_type,
@@ -57,7 +76,9 @@ def health_check():
     return {"data":200}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     uvicorn.run("main:app", host="0.0.0.0", port=8080,
-                reload=True, debug=True, log_config="logging.conf"
+                reload=True, debug=True, log_config="/opt/logging.conf"
                 )
+
